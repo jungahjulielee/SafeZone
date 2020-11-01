3183853ddd7d80779139c2aa0c3cbddf9ca59645
roszk008 <roszk008@umn.edu>
2020-11-01 10:53:03 -0600

    weow now the mass text has pseudo-functionality but still looks like shit

--- app/src/main/java/com/example/safezone/MasstextFragment.java
+++ app/src/main/java/com/example/safezone/MasstextFragment.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 public class MasstextFragment extends Fragment {
@@ -14,5 +20,30 @@ public class MasstextFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_masstext, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Initialize spinner entries
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinnerRecipients);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.recipients_entries, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // Set button callback
+        Button send_button = (Button) view.findViewById(R.id.buttonSend);
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "SMS Sent", Toast.LENGTH_SHORT).show();
+                EditText message = (EditText) view.findViewById(R.id.editTextMessage);
+                message.setText("");
+            }
+        });
     }
 }
