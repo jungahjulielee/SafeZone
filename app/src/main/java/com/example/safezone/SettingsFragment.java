c7f8e22a4742c6dcce1a04ea8880639f4d90f877
roszk008 <roszk008@umn.edu>
2020-11-19 11:33:41 -0600

    Settings page has content, doesn't really work tho

--- app/src/main/java/com/example/safezone/SettingsFragment.java
+++ app/src/main/java/com/example/safezone/SettingsFragment.java
@@ -4,15 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Initialize spinner entries
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.recipients_entries, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        RecyclerView recipients_recycler = view.findViewById(R.id.recyclerRecipients);
+        recipients_recycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        RecipientsAdapter recipients_adapter = new RecipientsAdapter(getResources().getStringArray(R.array.recipients_entries));
+        recipients_recycler.setAdapter(recipients_adapter);
     }
 }
